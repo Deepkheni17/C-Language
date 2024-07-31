a6edacdd7b35633477366dc44816eb6576350ff6
Deepkheni17 <deepkheni07@gmail.com>
2024-07-31 15:56:07 +0530

    Add files via upload

---
+++ onevalue.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	
+	int a,b,c,d;
+	a=b=c=d=2;
+	
+	printf("a = %d \n",a);
+	printf("b = %d \n",b);
+	printf("c = %d \n",c);
+	printf("d = %d \n",d);
+
+}
